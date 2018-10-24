9190b87fce68558ce82845853b9b62f8f8c421d6
Paul Du <du.paul136@gmail.com>
2018-10-24 11:08:59 +0200

    Simplification de move

--- main.pl
+++ main.pl
@@ -6,9 +6,17 @@ play(Board,Player,Result) :- chooseMove(Board,Player,Move),
                              nextPlayer(Player,Player1),
                              !,play(Board1,Player1,Result).
 
+diskAt(Board,IndexL,IndexC,Disk) :- nth1(IndexL,Board,BoardLine),nth1(IndexC,BoardLine,Disk).
+
+insideBoard(IndexL, IndexC) :- insideBoardL(IndexL), insideBoardC(IndexC).
+insideBoardL(IndexL) :- IndexL > 0, maxL(MaxL), IndexL =< MaxL.
+insideBoardC(IndexC) :- IndexC > 0, maxC(MaxC), IndexC =< MaxC.
+
 chooseMove(Board,Player,Move) :- setof(M,move(Board,Player,M),Moves),evaluateAndChoose(Moves,Board,(nil,-1000),Move).
 
 evaluateAndChoose([X|_],_,_,X).
+
+giveValue(V,V).
 
 applyMove((X,Y),0,Board,Boardl) :- nth1(X,Board,L),
                                    nth1(Y,L,0),
@@ -54,8 +62,6 @@ countTotalList([],_,V,V).
 juge(X,Y,0) :- X > Y,!.
 juge(_,_,1).
 
-
-
 init :- assert(maxL(8)),
         assert(maxC(8)),
         length(L1,8),
@@ -72,8 +78,8 @@ init :- assert(maxL(8)),
         nth1(5,L3,0),
         nth1(5,L2,0),
         nth1(5,L5,1),
-        nth1(4,L6,1),
-        nth1(4,L7,1),
+        nth1(4,L6,0),
+        nth1(4,L7,0),
         nth1(4,L8,1),
         nth1(6,L3,1),
         assert(dynamic board/1),
@@ -82,31 +88,30 @@ init :- assert(maxL(8)),
         board(Board).
 
 % generer un board à dimensions variables
-init2 :-
-		askForGameHeight(GameHeight),
-		assert(maxL(GameHeight)),
-		assert(maxC(GameHeight)),
-		makeMatrix(GameHeight,Mat),
-		Index1 is GameHeight / 2,
-		Index2 is 1 + GameHeight / 2,
-		placeDisk(Index1,Index1,Mat,0),
-		placeDisk(Index1,Index2,Mat,1),
-		placeDisk(Index2,Index1,Mat,1),
-		placeDisk(Index2,Index2,Mat,0),
-		[move],
+init2 :- askForGameHeight(GameHeight),
+         assert(maxL(GameHeight)),
+         assert(maxC(GameHeight)),
+         makeMatrix(GameHeight,Mat),
+         Index1 is GameHeight / 2,
+         Index2 is 1 + GameHeight / 2,
+         placeDisk(Index1,Index1,Mat,0),
+         placeDisk(Index1,Index2,Mat,1),
+         placeDisk(Index2,Index1,Mat,1),
+         placeDisk(Index2,Index2,Mat,0),
+         [move],
         assert(dynamic board/1),
         assert(board(Mat)).
 
 placeDisk(IndexL,IndexC,Board,Disk) :- nth1(IndexL,Board,BoardLine),nth1(IndexC,BoardLine,Disk).
 
 askForGameHeight(GameHeight):- write('Saisir la taille du jeu : '), read(Input),nl,
-							(Input mod 2 =:= 0 -> GameHeight is Input,!;var(GameHeight),
-							write("Taille invalide, veuillez saisir un nombre pair."),nl,askForGameHeight(GameHeight)).
+                               (Input mod 2 =:= 0 -> GameHeight is Input,!;var(GameHeight),
+                               write("Taille invalide, veuillez saisir un nombre pair."),nl,askForGameHeight(GameHeight)).
 
 makeMatrix(N, Mat) :- makeMatrix(N,N,Mat).
 makeMatrix(N, M, Mat) :- length(Mat,N),
-					 makeLine(Mat,M).
-					 
+                         makeLine(Mat,M).
+                     
 makeLine([],M):- !.
 makeLine([H|T],M):- length(H,M),
-					 makeLine(T,M).
+                    makeLine(T,M).
--- move.pl
+++ move.pl
@@ -2,150 +2,27 @@ countMoves(Board,Player,Number) :- setof(M,move(Board,Player,M),Moves),count(Mov
 count([X|L],N,Number) :- NewN is N+1, count(L,NewN,Number).
 count([],N,N).
 
+direction(-1,0).
+direction(1,0).
+direction(0,-1).
+direction(0,1).
+direction(-1,-1).
+direction(1,1).
+direction(-1,1).
+direction(1,-1).
 
-move(Board,Player,Result) :- accesElement(Board,IndexL,IndexC,Elem),nonvar(Elem),Elem=:=Player,vertical(Board,(IndexL,IndexC),Player,Result).
-move(Board,Player,Result) :- accesElement(Board,IndexL,IndexC,Elem),nonvar(Elem),Elem=:=Player,horizontal(Board,(IndexL,IndexC),Player,Result).
-move(Board,Player,Result) :- accesElement(Board,IndexL,IndexC,Elem),nonvar(Elem),Elem=:=Player,slash(Board,(IndexL,IndexC),Player,Result).
-move(Board,Player,Result) :- accesElement(Board,IndexL,IndexC,Elem),nonvar(Elem),Elem=:=Player,antiSlash(Board,(IndexL,IndexC),Player,Result).
+move(Board,Player,Move) :- diskAt(Board,IndexL,IndexC,Disk),nonvar(Disk),Disk=:=Player,direction(DL,DC),move(Board,(IndexL,IndexC),Player,(DL,DC),0,Move).
 
-
-accesElement(Board,IndexL,IndexC,E) :- nth1(IndexL,Board,L),nth1(IndexC,L,E).
-
-giveValue(V,V).
-
-vertical(Board,(IndexL,IndexC),Player,R) :- verticalUp(Board,(IndexL,IndexC),Player,0,R).
-vertical(Board,(IndexL,IndexC),Player,R) :- verticalDown(Board,(IndexL,IndexC),Player,0,R).
-horizontal(Board,(IndexL,IndexC),Player,R) :- horizontalLeft(Board,(IndexL,IndexC),Player,0,R).
-horizontal(Board,(IndexL,IndexC),Player,R) :- horizontalRight(Board,(IndexL,IndexC),Player,0,R).
-slash(Board,(IndexL,IndexC),Player,R) :- slashUp(Board,(IndexL,IndexC),Player,0,R).
-slash(Board,(IndexL,IndexC),Player,R) :- slashDown(Board,(IndexL,IndexC),Player,0,R).
-antiSlash(Board,(IndexL,IndexC),Player,R) :- antiSlashUp(Board,(IndexL,IndexC),Player,0,R).
-antiSlash(Board,(IndexL,IndexC),Player,R) :- antiSlashDown(Board,(IndexL,IndexC),Player,0,R).
-
-verticalUp(Board,(IndexL,IndexC),Player,Counter,R) :- Counter > 0,
-                                    NewIndexL is IndexL-1,
-                                    NewIndexL > 0,
-                                    accesElement(Board,NewIndexL,IndexC,E),
-                                    var(E),
-                                    giveValue((NewIndexL,IndexC),R),!.
-verticalUp(Board,(IndexL,IndexC),Player,Counter,R) :- NewIndexL is IndexL-1,
-                                    NewIndexL > 0,
-                                    accesElement(Board,NewIndexL,IndexC,E),
-                                    nonvar(E),
-                                    E=:=1-Player,
-                                    NewCounter is Counter+1,
-                                    verticalUp(Board,(NewIndexL,IndexC),Player,NewCounter,R).
-                                
-verticalDown(Board,(IndexL,IndexC),Player,Counter,R) :- Counter > 0,
-                                    NewIndexL is IndexL+1,
-                                    NewIndexL < 9,
-                                    accesElement(Board,NewIndexL,IndexC,E),
-                                    var(E),
-                                    giveValue((NewIndexL,IndexC),R),!.
-verticalDown(Board,(IndexL,IndexC),Player,Counter,R) :- NewIndexL is IndexL+1,
-                                    NewIndexL < 9,
-                                    accesElement(Board,NewIndexL,IndexC,E),
-                                    nonvar(E),
-                                    E=:=1-Player,
-                                    NewCounter is Counter+1,
-                                    verticalDown(Board,(NewIndexL,IndexC),Player,NewCounter,R).
-
-horizontalLeft(Board,(IndexL,IndexC),Player,Counter,R) :- Counter > 0,
-                                    NewIndexC is IndexC-1,
-                                    NewIndexC > 0,
-                                    accesElement(Board,IndexL,NewIndexC,E),
-                                    var(E),
-                                    giveValue((IndexL,NewIndexC),R),!.
-horizontalLeft(Board,(IndexL,IndexC),Player,Counter,R) :- NewIndexC is IndexC-1,
-                                    NewIndexC > 0,
-                                    accesElement(Board,IndexL,NewIndexC,E),
-                                    nonvar(E),
-                                    E=:=1-Player,
-                                    NewCounter is Counter+1,
-                                    horizontalLeft(Board,(IndexL,NewIndexC),Player,NewCounter,R).
-                                            
-horizontalRight(Board,(IndexL,IndexC),Player,Counter,R) :- Counter > 0,
-                                    NewIndexC is IndexC+1,
-                                    NewIndexC < 9,
-                                    accesElement(Board,IndexL,NewIndexC,E),
-                                    var(E),
-                                    giveValue((IndexL,NewIndexC),R),!.
-horizontalRight(Board,(IndexL,IndexC),Player,Counter,R) :- NewIndexC is IndexC+1,
-                                    NewIndexC < 9,
-                                    accesElement(Board,IndexL,NewIndexC,E),
-                                    nonvar(E),
-                                    E=:=1-Player,
-                                    NewCounter is Counter+1,
-                                    horizontalRight(Board,(IndexL,NewIndexC),Player,NewCounter,R).
-                                            
-slashUp(Board,(IndexL,IndexC),Player,Counter,R) :- Counter > 0,
-                                    NewIndexC is IndexC-1,
-                                    NewIndexL is IndexL-1,
-                                    NewIndexC > 0,
-                                    NewIndexL > 0,
-                                    accesElement(Board,NewIndexL,NewIndexC,E),
-                                    var(E),
-                                    giveValue((NewIndexL,NewIndexC),R),!.
-slashUp(Board,(IndexL,IndexC),Player,Counter,R) :- NewIndexC is IndexC-1,
-                                    NewIndexL is IndexL-1,
-                                    NewIndexC > 0,
-                                    NewIndexL > 0,
-                                    accesElement(Board,NewIndexL,NewIndexC,E),
-                                    nonvar(E),
-                                    E=:=1-Player,
-                                    NewCounter is Counter+1,
-                                    slashUp(Board,(NewIndexL,NewIndexC),Player,NewCounter,R).
-                                            
-slashDown(Board,(IndexL,IndexC),Player,Counter,R) :- Counter > 0,
-                                    NewIndexC is IndexC+1,
-                                    NewIndexL is IndexL+1,
-                                    NewIndexC < 9,
-                                    NewIndexL < 9,
-                                    accesElement(Board,NewIndexL,NewIndexC,E),
-                                    var(E),
-                                    giveValue((NewIndexL,NewIndexC),R),!.
-slashDown(Board,(IndexL,IndexC),Player,Counter,R) :- NewIndexC is IndexC+1,
-                                    NewIndexL is IndexL+1,
-                                    NewIndexC < 9,
-                                    NewIndexL < 9,
-                                    accesElement(Board,NewIndexL,NewIndexC,E),
-                                    nonvar(E),
-                                    E=:=1-Player,
-                                    NewCounter is Counter+1,
-                                    slashDown(Board,(NewIndexL,NewIndexC),Player,NewCounter,R).
-                                            
-antiSlashUp(Board,(IndexL,IndexC),Player,Counter,R) :- Counter > 0,
-                                    NewIndexC is IndexC+1,
-                                    NewIndexL is IndexL-1,
-                                    NewIndexC < 9,
-                                    NewIndexL > 0,
-                                    accesElement(Board,NewIndexL,NewIndexC,E),
-                                    var(E),
-                                    giveValue((NewIndexL,NewIndexC),R),!.
-antiSlashUp(Board,(IndexL,IndexC),Player,Counter,R) :- NewIndexC is IndexC+1,
-                                    NewIndexL is IndexL-1,
-                                    NewIndexC < 9,
-                                    NewIndexL > 0,
-                                    accesElement(Board,NewIndexL,NewIndexC,E),
-                                    nonvar(E),
-                                    E=:=1-Player,
-                                    NewCounter is Counter+1,
-                                    antiSlashUp(Board,(NewIndexL,NewIndexC),Player,NewCounter,R).
-                                            
-antiSlashDown(Board,(IndexL,IndexC),Player,Counter,R) :- Counter > 0,
-                                    NewIndexC is IndexC-1,
-                                    NewIndexL is IndexL+1,
-                                    NewIndexC > 0,
-                                    NewIndexL < 9,
-                                    accesElement(Board,NewIndexL,NewIndexC,E),
-                                    var(E),
-                                    giveValue((NewIndexL,NewIndexC),R),!.
-antiSlashDown(Board,(IndexL,IndexC),Player,Counter,R) :- NewIndexC is IndexC-1,
-                                    NewIndexL is IndexL+1,
-                                    NewIndexC > 0,
-                                    NewIndexL < 9,
-                                    accesElement(Board,NewIndexL,NewIndexC,E),
-                                    nonvar(E),
-                                    E=:=1-Player,
-                                    NewCounter is Counter+1,
-                                    antiSlashDown(Board,(NewIndexL,NewIndexC),Player,NewCounter,R).
+move(Board,(IndexL,IndexC),Player,(DirectionL,DirectionC),Counter,(MoveL,MoveC)) :- Counter > 0,
+                                                                                    MoveL is (IndexL+DirectionL),
+                                                                                    MoveC is (IndexC+DirectionC),
+                                                                                    insideBoard(MoveL,MoveC),
+                                                                                    diskAt(Board,MoveL,MoveC,Disk),
+                                                                                    var(Disk),!.
+move(Board,(IndexL,IndexC),Player,(DirectionL,DirectionC),Counter,Move) :- NewIndexL is (IndexL+DirectionL),
+                                                                           NewIndexC is (IndexC+DirectionC),
+                                                                           insideBoard(NewIndexL,NewIndexC),
+                                                                           diskAt(Board,NewIndexL,NewIndexC,Disk),
+                                                                           nonvar(Disk),
+                                                                           Disk=:=1-Player,
+                                                                           move(Board,(NewIndexL,NewIndexC),Player,(DirectionL,DirectionC),Counter+1,Move).
