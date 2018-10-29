83f84e0661133bca8ed23a2eef2d372dae087471
Kragzmo-Simon <fery.simon@ymail.com>
2018-10-29 17:05:22 +0100

    Les Joueurs peuvent dersormais etre humains.

--- main.pl
+++ main.pl
@@ -1,30 +1,42 @@
 %% --- Init ---
 init :- askForGameHeight(GameHeight),
-        init(GameHeight).
+        askForNumberOfPlayers(NbOfPlayers),
+        init(GameHeight,NbOfPlayers).
 
 askForGameHeight(GameHeight):- write('Saisir la taille du jeu : '), read(Input),nl,
-                        (Input mod 2 =:= 0 -> GameHeight is Input,!;var(GameHeight),
-                        write("Taille invalide, veuillez saisir un nombre pair."),nl,askForGameHeight(GameHeight)).
+                        (Input mod 2 =:= 0 -> GameHeight is Input,! 
+                                            ; var(GameHeight), write("Taille invalide, veuillez saisir un nombre pair."),nl,askForGameHeight(GameHeight)).
 
-init(GameHeight) :- GameHeight mod 2 =:= 0,
-                    [board],
-                    [move],
-                    [transformBoard],
-                    
-                    retractall(maxL(_)),
-                    retractall(maxC(_)),
-                    retractall(board(_)),
-                    
-                    assert(maxL(GameHeight)),
-                    assert(maxC(GameHeight)),
-                    makeMatrix(GameHeight,Mat),
-                    Index1 is GameHeight / 2,
-                    Index2 is 1 + GameHeight / 2,
-                    diskAt(Mat,Index1,Index1,0),
-                    diskAt(Mat,Index1,Index2,1),
-                    diskAt(Mat,Index2,Index1,1),
-                    diskAt(Mat,Index2,Index2,0),
-                    assert(board(Mat)).
+askForNumberOfPlayers(NbOfPlayers):-    write('Saisir le nombre de joueurs humains (0, 1 ou 2) : '),read(Input),nl,
+                                        ( (Input =< 2, Input >= 0) ->  NbOfPlayers is Input
+                                                    ; write('Nombre invalide, veuillez le resaisir.'),askForNumberOfPlayers(NbOfPlayers)).
+						
+
+init(GameHeight,NbOfPlayers) :- GameHeight mod 2 =:= 0,
+                                [board],
+                                [move],
+                                [transformBoard],
+                                
+                                retractall(isHuman(_)),
+                                (NbOfPlayers >= 1 -> write('Le joueur 0 est humain.'), nl, assert(isHuman(0)) ; write('Le joueur 0 est une IA.'),nl),
+                                (NbOfPlayers =:= 2 -> write('Le joueur 1 est humain.'), nl, assert(isHuman(1)) ; write('Le joueur 1 est une IA.'),nl),
+                                nl,
+                                
+                                retractall(maxL(_)),
+                                retractall(maxC(_)),
+                                retractall(board(_)),
+                                
+                                assert(maxL(GameHeight)),
+                                assert(maxC(GameHeight)),
+                                makeMatrix(GameHeight,Mat),
+                                
+                                Index1 is GameHeight / 2,
+                                Index2 is 1 + GameHeight / 2,
+                                diskAt(Mat,Index1,Index1,0),
+                                diskAt(Mat,Index1,Index2,1),
+                                diskAt(Mat,Index2,Index1,1),
+                                diskAt(Mat,Index2,Index2,0),
+                                assert(board(Mat)).
 
                     
 %% --- Play ---
@@ -52,7 +64,26 @@ displayLine([X|L]) :- write(X),write(' '),!,displayLine(L).
 
 
 %% --- Choose Move ---
-chooseMove(Board,Player,Move) :- allMoves(Board,Player,Moves),evaluateAndChoose(Moves,Board,(nil,-1000),Move).
+chooseMove(Board,Player,Move) :- (isHuman(Player) -> chooseMoveHuman(Board,Player,Move); chooseMoveIA(Board,Player,Move)).
+
+chooseMoveIA(Board,Player,Move) :- allMoves(Board,Player,Moves),evaluateAndChoose(Moves,Board,(nil,-1000),Move).
+
+chooseMoveHuman(Board,Player,Move) :-   allMoves(Board,Player,Moves),
+                                        write('Liste des coups disponibles : '),write(Moves),nl,
+                                        write('Choisissez un coup : '),nl,
+                                        askForMove(Move,Moves),
+                                        write('Vous avez choisi le coup ['),write(Move),write(']'),nl.
+
+askForMove(Move,Moves):- 
+                write('X : '),read(InputX),
+                write('Y : '),read(InputY),
+                (moveIsLegal((InputX,InputY),Moves) -> assignMove(Move,(InputX,InputY))
+                    ; write('Coup non valide, veuillez réiterer la saisie.'),nl,askForMove(Move,Moves)).                            
+                            
+moveIsLegal(Move,[Move|_]).
+moveIsLegal(Move,[_|RestOfMoves]):- moveIsLegal(Move,RestOfMoves).
+               
+assignMove(Move,Move).
 
 %% https://stackoverflow.com/questions/2261238/random-items-in-prolog
 %% choose(List, Elt) - chooses a random element
@@ -89,4 +120,4 @@ judge(X,Y,0) :- X > Y.
 judge(X,Y,1) :- X < Y.
 judge(_,_,2).
 
-announce(Result):- write("Et le gagnant est ... le joueur des "),write(Result),write(" !"),nl.
+announce(Result):- write("Et le gagnant est ... le joueur "),write(Result),write("!"),nl.
