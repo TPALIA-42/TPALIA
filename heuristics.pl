99f719ab929571de09936f2977aebb41e93b5818
= <baptiste.thivend@insa-lyon.fr>
2018-11-06 07:14:13 +0100

    Minimal comments for heuristics

--- heuristics.pl
+++ heuristics.pl
@@ -1,45 +1,53 @@
-%% --- Naive AI ---
+%% --- Naive AI : Take the first move available ---
+%% > naiveAI/2 : +Moves, -Move
 naiveAI(Moves,Move) :-
     nth1(1,Moves,Move).
 
-%% --- Random AI ---
+%% --- Random AI : take random move ---
+%% randomChoose/2 : +List, -Elt
 randomChoose([], []).
 randomChoose(List, Elt) :-
     length(List, Length),
     random(0, Length, Index),
     nth0(Index, List, Elt).
 
-%% --- Simple AI ---
+%% --- Simple AI : Move that harvest the greatest number of disks ---
 simpleChoose(Moves,Player,Board,Move) :- simpleChoose(Moves,Player,Board,nil,-10000,Move).
 
 simpleChoose([],_,_,Best,_,Best).
 simpleChoose([Move|Moves],Player,Board,Actual,Value,Best) :-
-                                applyMove(Move,Player,Board,NewBoard),
-                                countTotalDisk(NewBoard,Player,N),
-                                (N > Value -> simpleChoose(Moves,Player,Board,Move,N,Best) ; simpleChoose(Moves,Player,Board,Actual,Value,Best)).
+    applyMove(Move,Player,Board,NewBoard),
+    countTotalDisk(NewBoard,Player,N),
+    (N > Value -> simpleChoose(Moves,Player,Board,Move,N,Best) ; simpleChoose(Moves,Player,Board,Actual,Value,Best)).
 
 %% --- MinMax heuristic ---
 minimaxChoose(Moves,Player,Board,Depth,Move) :-
-                                Counter is 0,
-                                MaxMin is 1,
-                                minimaxChoose(Moves,Player,Board,Board,Counter,Depth,MaxMin,(nil,-10000),(Move,_)).
+    Counter is 0,
+    MaxMin is 1,
+    minimaxChoose(Moves,Player,Board,Board,Counter,Depth,MaxMin,(nil,-10000),(Move,_)).
+
 minimaxChoose([Move|Moves],Player,Board,OriginalBoard,Counter,Depth,MaxMin,Record,Best) :-
-                                applyMove(Move,Player,Board,NewBoard),
-                                minimax(Depth,Player,NewBoard,OriginalBoard,MaxMin,_,Value),
-                                update(Move,Value,Record,NewRecord,MaxMin,Counter),
-                                NewCounter is Counter+1,
-                                minimaxChoose(Moves,Player,Board,OriginalBoard,NewCounter,Depth,MaxMin,NewRecord,Best).
+    applyMove(Move,Player,Board,NewBoard),
+    minimax(Depth,Player,NewBoard,OriginalBoard,MaxMin,_,Value),
+    update(Move,Value,Record,NewRecord,MaxMin,Counter),
+    NewCounter is Counter+1,
+    minimaxChoose(Moves,Player,Board,OriginalBoard,NewCounter,Depth,MaxMin,NewRecord,Best).
+
 minimaxChoose([],_,_,_,_,_,_,Record,Record) :- !.
 
-minimax(0,Player,Board,OriginalBoard,MaxMin,_,Value) :- value(Player,Board,OriginalBoard,0,V),Value is V*MaxMin,!.
+minimax(0,Player,Board,OriginalBoard,MaxMin,_,Value) :-
+    value(Player,Board,OriginalBoard,0,V),
+    Value is V*MaxMin,!.
 
-minimax(D,Player,Board,OriginalBoard,MaxMin,Move,Value) :- D > 0,
-                                NewPlayer is 1-Player,
-                                D1 is D-1,
-                                MinMax is -1*MaxMin,
-                                setof(M,move(Board,NewPlayer,M),Moves),
-                                !,
-                                minimaxChoose(Moves,NewPlayer,Board,OriginalBoard,0,D1,MinMax,(nil,-10000),(Move,Value)).
+minimax(D,Player,Board,OriginalBoard,MaxMin,Move,Value) :-
+    D > 0,
+    NewPlayer is 1-Player,
+    D1 is D-1,
+    MinMax is -1*MaxMin,
+    setof(M,move(Board,NewPlayer,M),Moves),
+    !,
+    minimaxChoose(Moves,NewPlayer,Board,OriginalBoard,0,D1,MinMax,(nil,-10000),(Move,Value)).
+
 minimax(_,Player,Board,_,MaxMin,nil,Value) :-
     countTotalDisk(Board,Player,N),
     Opponent is 1-Player,
@@ -54,35 +62,36 @@ update(_,Value,(Move1,Value1),(Move1,Value1),-1,_) :- Value >= Value1.
 
 %% ---AlphaBeta heuristic ---
 alphaBetaChoose(Moves,Player,Board,Depth,Move) :-
-                                Alpha is -10000,
-                                Beta is 10000,
-                                nth1(1,Moves,FirstMove),
-                                alphaBetaChoose(Moves,Player,Board,Board,Depth,Alpha,Beta,FirstMove,(Move,_)).
+    Alpha is -10000,
+    Beta is 10000,
+    nth1(1,Moves,FirstMove),
+    alphaBetaChoose(Moves,Player,Board,Board,Depth,Alpha,Beta,FirstMove,(Move,_)).
 
 alphaBetaChoose([Move|Moves],Player,Board,OriginalBoard,Depth,Alpha,Beta,Record,Best) :-
-                                                                    applyMove(Move,Player,Board,NewBoard),
-                                                                    alphaBeta(Depth,Player,NewBoard,OriginalBoard,Alpha,Beta,_,Value),
-                                                                    cutoff(Move,Value,Player,Board,OriginalBoard,Depth,Alpha,Beta,Moves,Record,Best).
+    applyMove(Move,Player,Board,NewBoard),
+    alphaBeta(Depth,Player,NewBoard,OriginalBoard,Alpha,Beta,_,Value),
+    cutoff(Move,Value,Player,Board,OriginalBoard,Depth,Alpha,Beta,Moves,Record,Best).
+
 alphaBetaChoose([],_,_,_,_,Alpha,_,Record,(Record,Alpha)) :- !.
 
 alphaBeta(0,Player,Board,OriginalBoard,_,_,_,Value) :- value(Player,Board,OriginalBoard,0,Value),!.
 
-alphaBeta(D,Player,Board,OriginalBoard,Alpha,Beta,Move,Value) :- D > 0,
-                                                    NewPlayer is 1-Player,
-                                                    D1 is D-1,
-                                                    Alpha1 is -1*Beta,
-                                                    Beta1 is -1*Alpha,
-                                                    setof(M,move(Board,NewPlayer,M),Moves),
-                                                    !,
-                                                    alphaBetaChoose(Moves,NewPlayer,Board,OriginalBoard,D1,Alpha1,Beta1,nil,(Move,Value1)),
-                                                    Value is -1*Value1.
+alphaBeta(D,Player,Board,OriginalBoard,Alpha,Beta,Move,Value) :-
+    D > 0,
+    NewPlayer is 1-Player,
+    D1 is D-1,
+    Alpha1 is -1*Beta,
+    Beta1 is -1*Alpha,
+    setof(M,move(Board,NewPlayer,M),Moves),!,
+    alphaBetaChoose(Moves,NewPlayer,Board,OriginalBoard,D1,Alpha1,Beta1,nil,(Move,Value1)),
+    Value is -1*Value1.
 
 alphaBeta(_,Player,Board,_,Alpha,Beta,_,Value) :-
     countTotalDisk(Board,Player,N),
     Opponent is 1-Player,
     countTotalDisk(Board,Opponent,NO),
     (N > NO -> Value is Beta ; Value is Alpha),!.
-                                                    
+
 cutoff(Move,Value,_,_,_,_,_,Beta,_,_,(Move,Value)) :-
     Value >= Beta,!.
 
